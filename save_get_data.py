3ba09bcfbff38f87dc024efa6be378f9405dc23e
RazGrom <grom02.03.5@mail.ru>
2020-11-02 21:45:25 +0300

    created

---
+++ save_get_data.py
@@ -0,0 +1,18 @@
+def save_data(file):
+    import pickle
+    with open('data.picle', 'wb') as f:
+        pickle.dump(file,f)
+
+
+def load_data():
+    import  pickle
+    with open('data.picle', 'rb') as r:
+        users = pickle.load(r)
+    return users
+
+if __name__ == '__main__':
+    data = {'121':'srfd'}
+    save_data(data)
+    del data
+    data = load_data()
+    print(data)
