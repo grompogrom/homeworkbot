ad9ed71fe1747e056ddea7e9fd086525877e5cc9
RazGrom <grom02.03.5@mail.ru>
2020-11-05 22:46:52 +0300

    resized keyboards

--- keyboards.py
+++ keyboards.py
@@ -1,23 +1,23 @@
 from telebot import types
 
-start_keyboard = types.ReplyKeyboardMarkup()
+start_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
 
 item_check = types.KeyboardButton('Узнать дз')
 item_add = types.KeyboardButton('Добавить дз')
 start_keyboard.add(item_check, item_add)
 
-days_keyboard = types.ReplyKeyboardMarkup()
+days_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
 days_keyboard.add('пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'готово')
 
-ready_keyboard = types.ReplyKeyboardMarkup(row_width=1)
+ready_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
 ready_keyboard.add('готово')
 
-add_week_keyboard = types.ReplyKeyboardMarkup()
+add_week_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
 add_week_keyboard.add('Добавить числитель', 'Нет')
 
 
 def create_lessons_keyboard(lessons: list):
-    lessons_keyboard = types.ReplyKeyboardMarkup()
+    lessons_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     for lesson in lessons:
         button = types.KeyboardButton(lesson)
         lessons_keyboard.add(button)
@@ -25,17 +25,17 @@ def create_lessons_keyboard(lessons: list):
 
 
 def create_choose_day_keyboard(days: list):
-    chack_lessons_keyboard = types.ReplyKeyboardMarkup()
+    check_lessons_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     button = types.KeyboardButton('Thanks')
-    chack_lessons_keyboard.add(button)
-    chack_lessons_keyboard.row(days)
+    check_lessons_keyboard.add(button)
+    check_lessons_keyboard.row(days)
     button = types.KeyboardButton('Выбрать предмет')
-    chack_lessons_keyboard.add(button)
-    return chack_lessons_keyboard
+    check_lessons_keyboard.add(button)
+    return check_lessons_keyboard
 
 
 def create_groups_keyboard(groups):
-    groups_keyboard = types.ReplyKeyboardMarkup()
+    groups_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     for group in groups:
         button = types.KeyboardButton(group)
         groups_keyboard.add(button)
@@ -45,8 +45,13 @@ def create_groups_keyboard(groups):
 
 
 def create_check_homework_keyboard(days):
-    check_hmwk_keyboard = types.ReplyKeyboardMarkup()
+    check_hmwk_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     check_hmwk_keyboard.add('Суппер!')
     for day in days:
-        check_hmwk_keyboard.row(day)
+        check_hmwk_keyboard.add(day)
     check_hmwk_keyboard.add('Выбрать предмет')
+    return check_hmwk_keyboard
+
+
+if __name__ == '__main__':
+    create_check_homework_keyboard(['пн', 'вт', 'ср', 'чт', 'пт', 'сб'])
