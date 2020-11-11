2a9fba6058e14cd9774661edc49d8f6e5ce7742b
RazGrom <grom02.03.5@mail.ru>
2020-11-11 22:13:30 +0300

    add_suffix function added

--- tools.py
+++ tools.py
@@ -7,5 +7,16 @@ def message_parser(json):
     return text
 
 
+def add_suffix(week, suffix):
+    rweek = []
+    for day in week:
+        day += suffix
+        rweek.append(day)
+    return rweek
+
+
 if __name__ == '__main__':
-    pass
+    days = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб']
+    days0 = add_suffix(days, ' ч')
+    d = days0[1][3]
+    print(d)
