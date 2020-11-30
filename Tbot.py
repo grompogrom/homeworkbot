eab78821319e3a98b533820d6b3a986fbcf73a4a
RazGrom <grom02.03.5@mail.ru>
2020-11-30 21:44:26 +0300

    added options menu

--- Tbot.py
+++ Tbot.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import telebot
 from config import TOKEN
-from chat_logic import chating, begining
+from chat_logic import chating, begining, again_registation
 
 
 bot = telebot.TeleBot(TOKEN)
@@ -19,7 +19,23 @@ def get_text_mess(message):
     chat_id = message.from_user.id
     answer = chating(chat_id, message.text)
     print(answer)
-    bot.send_message(chat_id, answer[0], reply_markup= answer[1])
+    if len(answer) == 3:
+        bot.send_message(chat_id, answer[0], reply_markup=answer[1], )
+    else:
+        bot.send_message(chat_id, answer[0], reply_markup= answer[1])
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def query_handler(call):
+    answer = ''
+    chat_id = call.from_user.id
+    if call.data == 'cb_rereg':
+        answer = again_registation(chat_id)
+    elif call.data == 'cb_history':
+        answer = '(Архив дз) Функция пока в разработке'
+    elif call.data == 'cb_feedback':
+        answer = '(Отзыв) Функция пока в разработке'
+    bot.send_message(chat_id, answer[0], reply_markup=answer[1])
 
 
 try:
